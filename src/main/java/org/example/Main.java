c87aeef8066991d86d80bb72209a69a689579098
igor-prys <photo.prys@gmail.com>
2023-02-01 21:51:53 +0200

    add java alike hashMap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,48 @@
 package org.example;
 
 import org.example.structures.CustomLinkedList;
+import org.example.structures.JavaAlikeHashSet;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        CustomLinkedList <Integer> list=new CustomLinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(5);
-        list.addByIndex(3,4);
-        list.removeElement(4);
-        System.out.println(list);
-        System.out.println(list.getCounter());
-        System.out.println(list.contain(8));
-
-       // System.out.println("Value "+list.getValue(4));
-        System.out.println();
-
-        List<Integer>list1=new LinkedList<>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        list1.add(5);
-        list1.add(3,4);
-        list1.remove(4);
-        System.out.println(list1);
-        System.out.println(list1.size());
-
+//        CustomLinkedList <Integer> list=new CustomLinkedList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(5);
+//        list.addByIndex(3,4);
+//        list.removeElement(4);
+//        System.out.println(list);
+//        System.out.println(list.getCounter());
+//        System.out.println(list.contain(8));
+//
+//       // System.out.println("Value "+list.getValue(4));
+//        System.out.println();
+//
+//        List<Integer>list1=new LinkedList<>();
+//        list1.add(1);
+//        list1.add(2);
+//        list1.add(3);
+//        list1.add(5);
+//        list1.add(3,4);
+//        list1.remove(4);
+//        System.out.println(list1);
+//        System.out.println(list1.size());
+//        System.out.println();
+//
+//        Map<Integer,String>map=new HashMap<>();
+//        map.put(1,"a");
+//        map.put(2,null);
+//        System.out.println(map.containsKey(3));
+//        System.out.println(map.get(3));
+        JavaAlikeHashSet<String>jahs=new JavaAlikeHashSet<>(15);
+        jahs.add(null);
+        jahs.add("bbbbb");
+        System.out.println(jahs);
     }
 }
---
+++ src/test/java/org/example/structures/JavaAlikeHashSetTest.java
@@ -0,0 +1,40 @@
+package org.example.structures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JavaAlikeHashSetTest {
+    private JavaAlikeHashSet jhs;
+
+    @BeforeEach
+    public void before(){
+        jhs=new JavaAlikeHashSet();
+    }
+    @Test
+    public void shouldCheckMethodAdd() {
+        jhs.add("a");
+        jhs.add("b");
+        jhs.add("c");
+        jhs.add("d");
+        jhs.add("e");
+        jhs.add("f");
+        Assertions.assertTrue(jhs.contains("c"));
+        Assertions.assertFalse(jhs.contains("t"));
+        Assertions.assertEquals(6, jhs.getSize());
+    }
+
+    @Test
+    public void shouldIgnoreDuplicate() {
+        jhs.add("a");
+        jhs.add("a");
+        Assertions.assertEquals("a", jhs.toString());
+    }
+
+//    @Test
+//    public void shouldAllowToAddNull() {
+//        jhs.add(null);
+//        jhs.add("a");
+//        Assertions.assertEquals("null a", jhs.toString());
+//    }
+}
